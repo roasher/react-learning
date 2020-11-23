c880ddb9faec7c83d04dccf6a0a2f0bd3c25d5ea
roasher <woodythegreat@yandex.ru>
2020-11-23 18:57:32 +0300

    add styles

--- src/components/common/products/product-item/product-item.js
+++ src/components/common/products/product-item/product-item.js
@@ -1,6 +1,18 @@
 import React from "react";
 import styles from './product-item.module.scss'
 
-export const ProductItem = ({product}) => (
-  <div className={styles.productItem}>{product.title}</div>
+export const ProductItem = ({id, title, image, price, description, toggleCart}) => (
+  <div className={styles.productItem}>
+    <picture>
+      <img
+        className={styles.image}
+        src={image}
+        alt={title}
+        itemProp={title}
+        loading="lazy"/>
+    </picture>
+    <div>{price}</div>
+    <div>{title}</div>
+    <button className={styles.button} onClick={() => toggleCart(id)}>Add</button>
+  </div>
 )
--- src/components/common/products/product-list/product-list.js
+++ src/components/common/products/product-list/product-list.js
@@ -1,12 +1,16 @@
 import React from "react";
-import styles from "./product-list.module.scss"
 import {ProductItem} from "../product-item";
+import styles from "./product-list.module.scss"
 
 export const ProductList = ({products = [], toggleCart, cart}) => (
   <div className={styles.productList}>
     {products.data.map(product => {
       return (
-        <ProductItem key={product.id} product={product}/>
+        <ProductItem
+          key={product.id}
+          toggleCart={toggleCart}
+          {...product}
+        />
       )
     })}
   </div>
