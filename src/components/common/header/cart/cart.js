2b00ada45bfe6e884558cf2ad8640562c84ed9a9
roasher <woodythegreat@yandex.ru>
2020-12-04 00:26:56 +0300

    add set state hook

--- src/components/common/header/cart/cart.js
+++ src/components/common/header/cart/cart.js
@@ -1,42 +1,34 @@
-import React from "react"
+import React, {useState} from "react"
 import styles from "./cart.module.scss"
 import {getCartSelector, removeFromCartAction} from "../../../../store/cart";
 import {connect} from "react-redux";
 
-class CartView extends React.Component {
-  state = {
-    isShow: false
-  }
+export const CartView = ({cartData: products, removeItemFromCart}) => {
+  const [isShow, setState] = useState(false);
 
-  toggleWindow = () => {
-    const products = this.props.cartData;
+  const toggleWindow = () => {
     if (products && products.length) {
-      this.setState(prevState => ({isShow: !prevState.isShow}))
+      setState(!isShow);
     }
-  }
+  };
 
-  render = () => {
-    const products = this.props.cartData;
-    const {removeItemFromCart} = this.props;
-    const {isShow} = this.state;
-    return (
-      <div className={styles.cart}>
-        <button onClick={this.toggleWindow}>Cart ({products.length})</button>
-        {isShow && products && products.length > 0 && (
-          <div className={styles.cartWindow}>
-            {products.map(product => (
-              <div key={product.id}
-                   className={styles.cartItem}>
-                <span className={styles.cartItemName}>{product.title}</span>
-                <button onClick={() => removeItemFromCart(product.id)}>x</button>
-              </div>
-            ))}
-          </div>
-        )}
-        <div>Total price: {products.map(product => product.price).reduce((a, b) => a + b, 0)}</div>
-      </div>
-    )
-  }
+  return (
+    <div className={styles.cart}>
+      <button onClick={toggleWindow}>Cart ({products.length})</button>
+      {isShow && products && products.length > 0 && (
+        <div className={styles.cartWindow}>
+          {products.map(product => (
+            <div key={product.id}
+                 className={styles.cartItem}>
+              <span className={styles.cartItemName}>{product.title}</span>
+              <button onClick={() => removeItemFromCart(product.id)}>x</button>
+            </div>
+          ))}
+        </div>
+      )}
+      <div>Total price: {products.map(product => product.price).reduce((a, b) => a + b, 0)}</div>
+    </div>
+  )
 }
 
 const mapStateToProps = (store) => ({
